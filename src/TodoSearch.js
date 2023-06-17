c87cfc8c74ae266104e707be389b97781b22556e
Alan Cabrera <holasoyalan9@gmail.com>
2023-06-17 02:50:07 -0600

    Clase 7. Aprendiendo a usar el estado y aplicandolo a nuestro TodoSearch para guardar lo que los usuarios escriben en el

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,13 +1,20 @@
+import React from "react";
 import "./TodoSearch.css"
 
 function TodoSearch() {
+  // const [state, setState] = React.useState('');
+  // setState('Nuevo valor');
+  const [searchValue, setSearchValue] = React.useState('');
+  console.log(`Buscando: ${searchValue}`);
   return (
     <input
       className="TodoSearch"  
       placeholder="Cortar Cebolla"
+      value={searchValue}
       onChange={(event) => {
-        console.log(event);
-        console.log(event.target.value);
+        // console.log(event);
+        // console.log(event.target.value);
+        setSearchValue(event.target.value);
       }}
     />
   );
